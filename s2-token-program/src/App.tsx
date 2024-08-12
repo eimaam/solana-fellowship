9f43ebad3983e533f925aed4be1d9d7b3614b1d1
eimaam <imamddahir@gmail.com>
2024-08-12 11:58:58 +0100

    refactor: Remove unnecessary files and components, update UI with ANTD library
    This commit removes the unused App.tsx, index.css, and main.tsx files. It also removes the WalletConnection component, which is no longer needed, and adds the PageLayout component to provide a layout for the pages in the application. The UI has been updated using the ANTD library, and the application now supports dark mode. Additionally, the WelcomeScreen component has been added to display a welcome message and a button to connect the wallet. The TokenContext and WalletContext components have also been added to manage token and wallet information. Finally, the tailwind.config.js and webpack.config.ts files have been updated.

--- s2-token-program/src/App.tsx
+++ s2-token-program/src/App.tsx
@@ -1,28 +1,114 @@
-// src/App.tsx
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CreateToken from './components/CreateToken';
 import MintToken from './components/MintToken';
 import TransferToken from './components/TransferToken';
 import BurnToken from './components/BurnToken';
 import DelegateToken from './components/DelegateToken';
-import WalletConnection from './components/WalletConnection';
+import { ConfigProvider, theme } from 'antd';
 
 // Import Buffer from the 'buffer' package
 import { Buffer } from 'buffer/';
-import WalletProviderComponent from './components/WalletConnection';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { Button } from 'antd';
+import { Layout } from 'antd';
+import PageLayout from './components/layout';
+import { useWalletModal, WalletModal } from '@solana/wallet-adapter-react-ui';
+import WelcomeScreen from './components/WelcomeScreen';
+import { Space } from 'antd';
+
+const { defaultAlgorithm, darkAlgorithm } = theme;
 
 
 const App: React.FC = () => {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+  
+  const [mounted, setMounted] = React.useState(false);
+
+  const { publicKey, disconnect, disconnecting } = useWallet()
+  const {setVisible} = useWalletModal()
+
+  const handleConnectWallet = async () => {
+    if(publicKey) {
+      disconnect()
+    } else {
+
+      setVisible(true)
+
+    }
+
+  }
+
+
+  useEffect(() => {
+    // AOS.init({
+    //   delay: 0,
+    //   duration: 700,
+    //   easing: 'ease-in-out',
+    // });
+    if (isDarkMode) {
+      document.body.classList.add('dark');
+    } else {
+      document.body.classList.add('light');
+      document.body.classList.remove('dark');
+    }
+  }, [isDarkMode, mounted]); 
+
+
+  useEffect(() => {
+    setMounted(true);
+  }, [theme]);
+
+  if (!mounted || !theme) return null;
+
   return (
-      <div className="App">
-        <h1>Solana Token Manager</h1>
-        <CreateToken />
-        <MintToken />
-        <TransferToken />
-        <BurnToken />
-        <DelegateToken />
+    <ConfigProvider
+  message={{
+    className: "text-white",
+  }}
+    theme={{
+      ...theme,
+      token: {
+        colorPrimary: isDarkMode ? "#fff" : "#0E0E0E",
+        colorText: isDarkMode ? "#fff" : "#0E0E0E",
+        colorLink: isDarkMode ? "#fff" : "#0E0E0E",
+      },
+      components: {
+        Button: {
+          algorithm: true,
+          colorPrimary: isDarkMode ? "#0078D4" : "#0E0E0E",
+          primaryColor: isDarkMode ? "#fff" : "#000",
+          defaultHoverColor: isDarkMode ? "#0E0E0E" : "#fff",
+          defaultHoverBg: isDarkMode ? "#fff" : "#0E0E0E",
+        },
+        Alert: {
+          colorText: isDarkMode ? "#fff" : "#0E0E0E",
+        },
+        Input: {
+          paddingBlock: 10
+        },
+        Modal: {
+          algorithm: true,
+          colorBgBlur: isDarkMode ? "rgba(0, 0, 0, 0.9)" : "rgba(255, 255, 255, 0.9)"
+        },
+      },
+      algorithm: mounted ? (isDarkMode ? darkAlgorithm : defaultAlgorithm) : defaultAlgorithm,
+    }}
+  >
+      <div className='h-screen overflow-y-scroll py-12 flex flex-col items-center  gap-8'>
+        <WelcomeScreen />
+        {
+publicKey &&
+        <div className="grid grid-cols-2 gap-6 w-[60%] mx-auto">
+          <CreateToken />
+          <MintToken />
+          <TransferToken />
+          <BurnToken />
+          <DelegateToken />
+        </div>
+        }
       </div>
+      </ConfigProvider>
   );
 };
 
--- s2-token-program/src/main.tsx
+++ s2-token-program/src/main.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 
 // Import Buffer from the 'buffer' package
 import { Buffer } from 'buffer/';
 import process from 'process'
-import WalletProviderComponent from './components/WalletConnection.tsx'
+import WalletContextProvider from './components/context/WalletContext.tsx';
+import "./App.css"
+import "./index.css"
+import { TokenProvider } from './components/context/TokenContext.tsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-  <WalletProviderComponent>
+  <WalletContextProvider>
+    <TokenProvider>
     <App />
-  </WalletProviderComponent>
+    </TokenProvider>
+  </WalletContextProvider>
   </BrowserRouter>,
 )
